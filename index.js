d69de8e84d992e8a4a54f93f5f3f2dab763cf82f
Khadiza Rafa <khadiza301rafa@gmail.com>
2020-10-06 05:51:16 +0600

    added environment variable for db_name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { response } = require('express');
 const { ObjectID } = require('mongodb');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r5dyr.mongodb.net/volunteer-network?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r5dyr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
